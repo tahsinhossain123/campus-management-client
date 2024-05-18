e2502e2f685293b7730d80639b21eae0a970c356
dinastyk <116314638+dinastyk@users.noreply.github.com>
2024-05-17 20:57:39 -0400

    add campus

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -1,15 +1,16 @@
 /*==================================================
-/src/components/views/index.js
+/src/components/containers/index.js
 
-This is a "barrel" file for the View components, which combines all the exports of individual Views
-and makes it easier to be used by the corresponding Container components.
+This is a "barrel" file for the Container components, which combines all the exports of individual Containers
+and makes it easier to import into App.js.
 
 Note: A "barrel" file is a way to rollup exports from other modules into a single convenient module. 
 The "barrel" (module) file re-exports the exports of other modules.
 ================================================== */
-export { default as AllCampusesView } from "./AllCampusesView";
-export { default as AllStudentsView } from "./AllStudentsView";
-export { default as NewStudentView } from "./NewStudentView";
-export { default as CampusView } from "./CampusView";
-export { default as StudentView } from "./StudentView";
-export { default as HomePageView } from "./HomePageView";
+export { default as HomePageContainer } from "./HomePageContainer";
+export { default as AllCampusesContainer } from "./AllCampusesContainer";
+export { default as CampusContainer } from "./CampusContainer";
+export { default as AllStudentsContainer } from "./AllStudentsContainer";
+export { default as StudentContainer } from "./StudentContainer";
+export { default as NewStudentContainer } from "./NewStudentContainer";
+export { default as EditStudentsContainer } from "./EditStudentsContainer";
